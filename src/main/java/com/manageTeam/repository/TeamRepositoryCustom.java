785b72aa0b9be553a77e90222092ea1b9d3e1f0a
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-06-27 17:59:06 +0900

    fix: Util.class 삭제 불필요

--- src/main/java/com/manageTeam/repository/TeamRepositoryCustom.java
+++ src/main/java/com/manageTeam/repository/TeamRepositoryCustom.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Pageable;
 
 import com.manageTeam.dto.MemberConditionDto;
 import com.manageTeam.dto.TeamDto;
-import com.manageTeam.entity.Position;
 
 public interface TeamRepositoryCustom {
 	public Page<TeamDto.Info> findAllByCondition(MemberConditionDto conditionDto, Pageable pageable);
