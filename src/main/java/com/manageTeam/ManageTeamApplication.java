57b3101202508e8c3f79b24c51331b31fb11ae82
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-06-27 12:32:26 +0900

    Start develop

---
+++ src/main/java/com/manageTeam/ManageTeamApplication.java
@@ -0,0 +1,26 @@
+package com.manageTeam;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class ManageTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageTeamApplication.class, args);
+	}
+	
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
+	}
+
+
+}
---
+++ src/test/java/com/manageTeam/ManageTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manageTeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
