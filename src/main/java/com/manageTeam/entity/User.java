5407f40c78f4039f83e954c3d4a3b2197122faac
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-07-03 11:02:34 +0900

    feat: User 테이블 추가

---
+++ src/main/java/com/manageTeam/entity/User.java
@@ -0,0 +1,33 @@
+package com.manageTeam.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+
+/*
+ * 사용자 관리 테이블
+ * */
+@Entity
+@Getter
+public class User extends BaseEntity{
+	
+	@Id
+	private String userId;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "team_id")
+	private Team team;
+	private String password;
+	private String username;
+	private String rsdntRgnmb;
+	private String phone;
+	private Address address;
+	@Enumerated(EnumType.STRING)
+	private ActivateStatus activateStatus;
+	
+}
