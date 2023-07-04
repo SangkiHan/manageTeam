68f2985d9c97667f60088b47f878b5d8af5fae94
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-07-04 15:35:46 +0900

    fix: 팀목록조회도 팀조회와 동일한 이류로 leftjoin으로 변경

--- src/main/java/com/manageTeam/repository/impl/TeamRepositoryImpl.java
+++ src/main/java/com/manageTeam/repository/impl/TeamRepositoryImpl.java
@@ -66,7 +66,7 @@ public class TeamRepositoryImpl implements TeamRepositoryCustom{
 						team.activateStatus
 						))
 				.from(team)
-				.join(team.members, member)
+				.leftJoin(team.members, member)
 				.where(
 						teamnameEq(conditionDto.getTeamName()),
 						activatestatusEq(conditionDto.getActivateStatus()),
