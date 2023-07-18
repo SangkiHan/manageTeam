5ac6d1e87f557131134c20abe4d506a2102231fe
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-07-18 15:19:05 +0900

    fix: 권한 변경

--- src/test/java/com/manageTeam/ManageTeamApplicationTests.java
+++ src/test/java/com/manageTeam/ManageTeamApplicationTests.java
@@ -3,10 +3,8 @@ package com.manageTeam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import lombok.RequiredArgsConstructor;
 
 @SpringBootTest
-@RequiredArgsConstructor
 class ManageTeamApplicationTests {
 	
 
